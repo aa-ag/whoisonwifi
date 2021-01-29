aa7d782a2087d85f56f092c18814bb0ce71a7c05
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-29 17:56:28 -0600

    using who-is-on-my-wifi.py

--- who.py
+++ who.py
@@ -1,9 +1,16 @@
 ###--- IMPORTS ---###
-import scapy.all as scapy
+from who_is_on_my_wifi import *
 
+info = device()
 
-###--- NETWORK SCANNER ---###
-request = scapy.ARP()
-
-print(request.summary())
-print(request.show())
+print(f"PC: {info[0]}")
+print(f"PC Product-Name: {info[1]}")
+print(f"MAC Address: {info[2]}")
+print(f"IP Address (host): {info[3]}")
+print(f"IP Address: {info[4]}")
+print(f"PC HostName: {info[5]}")
+print(f"WiFI Name: {info[6]}")
+print(f"Gateway: {info[7]}")
+print(f"DNS 1: {info[8]}")
+print(f"DNS 2: {info[9]}")
+print(f"Password: {info[10]}")
